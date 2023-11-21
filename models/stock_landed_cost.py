5cec6a00a42a9e5d76e7c36812c87c38de6641b5
dulivu <GeorgeL1102@gmail.com>
2023-11-21 10:31:59 -0500

    [UPD] removing analytic_fields from documents

--- models/stock_landed_cost.py
+++ models/stock_landed_cost.py
@@ -10,9 +10,12 @@ class AdjustmentLines(models.Model):
     
     def _create_account_move_line(self, move, credit_account_id, debit_account_id, qty_out, already_out_account_id):
         res = super(AdjustmentLines, self)._create_account_move_line(move, credit_account_id, debit_account_id, qty_out, already_out_account_id)
-        if self.cost_id.vendor_bill_id.analytic_account_id:
-            for i in res:
-                i[2].update({
-                    'analytic_distribution': {str(self.cost_id.vendor_bill_id.analytic_account_id.id): 100}
-                })
+        if self.cost_id.vendor_bill_id:
+            landed_lines = self.cost_id.vendor_bill_id.invoice_line_ids.filtered(lambda l: l.is_landed_cost_line)
+            if landed_lines and landed_lines[0].analytic_distribution:
+                analytics = list(landed_lines[0].analytic_distribution.keys())
+                for i in res:
+                    i[2].update({
+                        'analytic_distribution': {str(analytics[0]): 100}
+                    })
         return res
--- models/stock_valuation_layer.py
+++ models/stock_valuation_layer.py
@@ -9,12 +9,14 @@ class StockValuationLayer(models.Model):
         res = super(StockValuationLayer, self)._validate_accounting_entries()
         for rec in self:
             purchase_id = rec.stock_move_id.picking_id.purchase_id
-            if purchase_id and purchase_id.analytic_account_id:
+            if purchase_id:
                 for line in rec.account_move_id.line_ids:
-                    line.analytic_distribution = {str(purchase_id.analytic_account_id.id): 100}
+                    analytic = list(purchase_id.order_line[0].analytic_distribution.keys())[0]
+                    line.analytic_distribution = {str(analytic): 100}
             
             sale_id = rec.stock_move_id.picking_id.sale_id
-            if sale_id and sale_id.o_analytic_account_id:
+            if sale_id:
                 for line in rec.account_move_id.line_ids:
-                    line.analytic_distribution = {str(sale_id.o_analytic_account_id.id): 100}
+                    analytic = list(sale_id.order_line[0].analytic_distribution.keys())[0]
+                    line.analytic_distribution = {str(analytic): 100}
         return res
