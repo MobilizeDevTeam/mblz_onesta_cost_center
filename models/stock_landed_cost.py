17df763ca3022cbd716d74b67fd388a5bf6f2028
dulivu <GeorgeL1102@gmail.com>
2023-11-30 16:09:56 -0500

    [FIX] landed cost, is_landed_costs_line

--- models/stock_landed_cost.py
+++ models/stock_landed_cost.py
@@ -11,7 +11,7 @@ class AdjustmentLines(models.Model):
     def _create_account_move_line(self, move, credit_account_id, debit_account_id, qty_out, already_out_account_id):
         res = super(AdjustmentLines, self)._create_account_move_line(move, credit_account_id, debit_account_id, qty_out, already_out_account_id)
         if self.cost_id.vendor_bill_id:
-            landed_lines = self.cost_id.vendor_bill_id.invoice_line_ids.filtered(lambda l: l.is_landed_cost_line)
+            landed_lines = self.cost_id.vendor_bill_id.invoice_line_ids.filtered(lambda l: l.is_landed_costs_line)
             if landed_lines and landed_lines[0].analytic_distribution:
                 analytics = list(landed_lines[0].analytic_distribution.keys())
                 for i in res:
