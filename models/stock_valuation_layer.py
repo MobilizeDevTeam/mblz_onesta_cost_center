37b25d04f17df3c6191bc4453bbff71ce9fb6e10
dulivu <GeorgeL1102@gmail.com>
2024-03-19 10:42:23 -0500

    [UPD] is digit validation

--- models/stock_valuation_layer.py
+++ models/stock_valuation_layer.py
@@ -6,6 +6,8 @@ from odoo.exceptions import UserError
 class StockValuationLayer(models.Model):
     _inherit = 'stock.valuation.layer'
     
+    
+    
     def _validate_accounting_entries(self):
         res = super(StockValuationLayer, self)._validate_accounting_entries()
         for rec in self:
@@ -14,12 +16,14 @@ class StockValuationLayer(models.Model):
                 for line in rec.account_move_id.line_ids:
                     if purchase_id.order_line[0].analytic_distribution:
                         analytic = list(purchase_id.order_line[0].analytic_distribution.keys())[0]
-                        line.analytic_distribution = {str(analytic): 100}
+                        if analytic.isdigit():
+                            line.analytic_distribution = {str(analytic): 100}
             
             sale_id = rec.stock_move_id.picking_id.sale_id
             if sale_id:
                 for line in rec.account_move_id.line_ids:
                     if sale_id.order_line[0].analytic_distribution:
                         analytic = list(sale_id.order_line[0].analytic_distribution.keys())[0]
-                        line.analytic_distribution = {str(analytic): 100}
+                        if analytic.isdigit():
+                            line.analytic_distribution = {str(analytic): 100}
         return res
